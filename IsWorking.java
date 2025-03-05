0a2b1d3ecd241b49085c3d51be3c88b7bd8858d2
creator36001 <Ofbruce01@bvsd.org>
2025-03-05 22:25:15 +0000

    added main, data.csv, speaker class (test for main), began development of IsWorking

---
+++ IsWorking.java
@@ -0,0 +1,28 @@
+
+//import data.csv;
+public class IsWorking{
+
+//String data = data.csv;
+
+
+public String[][] listData(String stringData){
+
+    int commaCount = 0;
+
+        for(int i = 0; i < stringData.length(); i++){
+            if (stringData.charAt(i) == 'b'){
+            commaCount++;
+            }
+        }
+    String[][] PackData = new String[commaCount/4][commaCount];
+
+        for(int i = 0; i < commaCount; i++){
+            if()
+        }
+
+    return PackData;
+}
+
+
+}
+
---
+++ Speaker.java
@@ -0,0 +1,9 @@
+
+//package sample1;
+
+public class Speaker{
+    public static void sayHello(){
+        System.out.println("hello World");
+    }
+
+}
